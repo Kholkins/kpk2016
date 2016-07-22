dd0f2557b67d9948dc2310ac0ac04eb2561298a5
Kholkinslab <kholkins@mail.ru>
2016-07-22 22:39:07 +0300

    1

--- gun/gun.py
+++ gun/gun.py
@@ -6,23 +6,23 @@ screen_height = 300
 timer_delay = 100
 
 class Ball:
- initial_number = 20
- minimal_radius = 15
- maximal_radius = 40
- available_colors = ['green', 'blue', 'red']
+    initial_number = 20
+    minimal_radius = 15
+    maximal_radius = 40
+    available_colors = ['green', 'blue', 'red']
 
     def __init__(self):
         """
- Cоздаёт шарик в случайном месте игрового холста canvas,
- при этом шарик не выходит за границы холста!
+     Cоздаёт шарик в случайном месте игрового холста canvas,
+     при этом шарик не выходит за границы холста!
         """
         R = randint(Ball.minimal_radius, Ball.maximal_radius)
- x = randint(0, screen_width-1-2*R)
- y = randint(0, screen_height-1-2*R)
+        x = randint(0, screen_width-1-2*R)
+        y = randint(0, screen_height-1-2*R)
         self._R = R
         self._x = x
         self._y = y
- fillcolor = choice(Ball.available_colors)
+        fillcolor = choice(Ball.available_colors)
         self._avatar = canvas.create_oval(x, y, x+2*R, y+2*R,
                                           width=1, fill=fillcolor,
                                           outline=fillcolor)
@@ -47,7 +47,7 @@ class Ball:
             self._y = screen_height - 2*self._R  - 1
             self._Vy = -self._Vy
 
- canvas.coords(self._avatar, self._x, self._y,
+        canvas.coords(self._avatar, self._x, self._y,
                       self._x + 2*self._R, self._y + 2*self._R)
 
 
@@ -63,15 +63,15 @@ class Gun:
 
     def shoot(self):
         """
- :return возвращает объект снаряда (класса Ball)
+      :return возвращает объект снаряда (класса Ball)
         """
- shell = Ball()
- shell._x = self._x + self._lx
- shell._y = self._y + self._ly
- shell._Vx = self._lx/10
- shell._Vy = self._ly/10
- shell._R = 5
- shell.fly()
+        shell = Ball()
+        shell._x = self._x + self._lx
+        shell._y = self._y + self._ly
+        shell._Vx = self._lx/10
+        shell._Vy = self._ly/10
+        shell._R = 5
+        shell.fly()
         return shell
 
 
@@ -81,36 +81,36 @@ def init_game():
  а также объект - пушку.
     """
     global balls, gun, shells_on_fly
- balls = [Ball() for i in range(Ball.initial_number)]
- gun = Gun()
- shells_on_fly = []
+    balls = [Ball() for i in range(Ball.initial_number)]
+    gun = Gun()
+    shells_on_fly = []
 
 def init_main_window():
     global root, canvas, scores_text, scores_value
- root = Tk()
- root.title("Пушка")
- scores_value = IntVar()
- canvas = Canvas(root, width=screen_width, height=screen_height,
+    root = Tk()
+    root.title("Пушка")
+    scores_value = IntVar()
+    canvas = Canvas(root, width=screen_width, height=screen_height,
                     bg="white")
- scores_text = Entry(root, textvariable=scores_value)
- canvas.grid(row=1, column=0, columnspan=3)
- scores_text.grid(row=0, column=2)
- canvas.bind('<Button-1>', click_event_handler)
+    scores_text = Entry(root, textvariable=scores_value)
+    canvas.grid(row=1, column=0, columnspan=3)
+    scores_text.grid(row=0, column=2)
+    canvas.bind('<Button-1>', click_event_handler)
 
 
 def timer_event():
     # все периодические рассчёты, которые я хочу, делаю здесь
     for ball in balls:
- ball.fly()
+        ball.fly()
     for shell in shells_on_fly:
- shell.fly()
- canvas.after(timer_delay, timer_event)
+        shell.fly()
+    canvas.after(timer_delay, timer_event)
 
 
 def click_event_handler(event):
     global shells_on_fly
- shell = gun.shoot()
- shells_on_fly.append(shell)
+    shell = gun.shoot()
+    shells_on_fly.append(shell)
 
 if __name__ == "__main__":
  init_main_window()
